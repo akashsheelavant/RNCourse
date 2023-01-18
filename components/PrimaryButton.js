dcc9069f148ca403dd6caf393b7995b0a0b17d5d
Akash <akashsheelavant4@gmail.com>
2023-01-18 16:21:34 +0530

    Adding buttons using custom component.

---
+++ components/PrimaryButton.js
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from "react-native"
+
+function PrimaryButton({ children }) {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>{children}</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default PrimaryButton
