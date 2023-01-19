248123bd71391acc3bbc62097d0656f2729caca4
Akash <akashsheelavant4@gmail.com>
2023-01-19 14:51:00 +0530

    Improving styles of button.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,8 +11,14 @@ function StartGameScreen() {
                 autoCapitalize="none"
                 autoCorrect={false}
             />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonContainer}>
+                <View style={styles.buttonViewInnerContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonViewInnerContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     )
 }
@@ -21,6 +27,8 @@ export default StartGameScreen
 
 const styles = StyleSheet.create({
     inputContainer: {
+        alignItems: "center",
+        justifyContent: "center",
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
@@ -42,5 +50,13 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row'
+    },
+    buttonViewInnerContainer: {
+        flex: 1
+        //width: 150,
     }
+
 })
