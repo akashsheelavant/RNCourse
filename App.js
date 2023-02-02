7042972d70637cc9d8292d979b342f060c62eff5
Akash <akashsheelavant4@gmail.com>
2023-02-02 13:49:17 +0530

    Using custom fonts.

--- App.js
+++ App.js
@@ -6,10 +6,21 @@ import { useState } from 'react';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/colors';
 import GameOverScreen from './screens/GameOverScreen';
+import { useFonts } from 'expo-font'
+import Apploading from 'expo-app-loading'
 
 export default function App() {
   const [enteredNumber, setEnteredNumber] = useState(null)
   const [isGameOver, setIsGameOver] = useState(true)
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return <Apploading />
+  }
 
   function selectedNumber(number) {
     setEnteredNumber(number)
