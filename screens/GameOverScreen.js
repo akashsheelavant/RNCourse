4596b95c171215335814ecb1468ad362266f1867
Akash <akashsheelavant4@gmail.com>
2023-05-14 12:10:01 +0530

    Further improvements on game over screen.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,19 +1,36 @@
-import { View, Text, Image, StyleSheet } from "react-native"
+import { View, Text, Image, StyleSheet, useWindowDimensions, ScrollView } from "react-native"
 import PrimaryButton from "../components/game/PrimaryButton"
 import Title from "../components/game/Title"
 import Colors from "../constants/colors"
 
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+    const { width, height } = useWindowDimensions()
+    let imageSize = 300
+    if (width < 380) {
+        imageSize = 150
+    }
+    if (height < 400) {
+        imageSize = 100
+    }
+
+    const imageStyle = {
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2
+    }
+
     return (
-        <View style={styles.rootContainer}>
-            <Title>GAME OVER!</Title>
-            <View style={styles.imageContainer}>
-                <Image style={styles.image} source={require('../assets/images/success.png')} />
+        <ScrollView style={styles.screen}>
+            <View style={styles.rootContainer}>
+                <Title>GAME OVER!</Title>
+                <View style={[styles.imageContainer, imageStyle]}>
+                    <Image style={styles.image} source={require('../assets/images/success.png')} />
+                </View>
+                <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlightedText}>{roundsNumber}</Text> rounds to guess number <Text style={styles.highlightedText}>{userNumber}</Text>
+                </Text>
+                <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
             </View>
-            <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlightedText}>{roundsNumber}</Text> rounds to guess number <Text style={styles.highlightedText}>{userNumber}</Text>
-            </Text>
-            <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-        </View>
+        </ScrollView>
 
     )
 }
@@ -21,6 +38,9 @@ function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
 export default GameOverScreen
 
 const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    },
     rootContainer: {
         flex: 1,
         padding: 24,
